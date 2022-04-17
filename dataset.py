4faf48f52641df88d8505d929ef6988738991f41
Bo Ai <albert_0110@outlook.com>
2022-04-17 08:03:40 +0800

    enforced the same label for doodle and real

--- dataset.py
+++ dataset.py
@@ -6,23 +6,26 @@ import numpy as np
 
 
 class ImageDataset(Dataset):
-    DATASET_DIR = 'dataset/dataset.npy'
-    DOODLE_KEY = 'google_doodle'
-    REAL_KEY = 'google_real'
+    DATASET_DIR = {True: 'dataset/dataset_train.npy', False: 'dataset/dataset_test.npy'}
+    DOODLE_KEY = 'sketchy_doodle'  # 'google_doodles'
+    REAL_KEY = 'sketchy_real'   # 'google_real'
 
-    def __init__(self, doodle_size, real_size):
+    def __init__(self, doodle_size, real_size, train: bool):
         super(ImageDataset, self).__init__()
-        dataset = np.load(self.DATASET_DIR, allow_pickle=True)[()]
+        dataset = np.load(self.DATASET_DIR[train], allow_pickle=True)[()]
         doodle, real = dataset[self.DOODLE_KEY], dataset[self.REAL_KEY]
+        self.doodle_dict = doodle
+        self.real_dict = real
 
         # sanity check
-        assert set(doodle.keys()) == set(real.keys()), f'doodle and real images label classes do not match'
+        assert doodle.keys() == real.keys(), f'doodle and real images label classes do not match'
 
         # process classes
         label_idx = {}
         for key in doodle.keys():
             if key not in label_idx:
                 label_idx[key] = len(label_idx)
+        self.label_idx = label_idx
 
         # parse data and labels
         self.doodle_data, self.doodle_label = self._return_x_y_pairs(doodle, label_idx)
@@ -42,7 +45,8 @@ class ImageDataset(Dataset):
             transforms.Normalize(self.real_data.mean(axis=(0,1,2)), self.real_data.std(axis=(0,1,2)))
         ])
 
-        print(f'Dataset. Doodle data size {len(self.doodle_data)}, real data size {len(self.real_data)}, '
+        print(f'Dataset {self.DOODLE_KEY} and {self.REAL_KEY}. '
+              f'Doodle data size {len(self.doodle_data)}, real data size {len(self.real_data)}, '
               f'ratio {len(self.doodle_data)/len(self.real_data)}')
 
     def _return_x_y_pairs(self, data_dict, category_mapping):
@@ -56,15 +60,17 @@ class ImageDataset(Dataset):
 
     def __getitem__(self, idx):
         # naive sampling scheme - sample with replacement
-        doodle_idx, real_idx = random.randint(0, len(self.doodle_data) - 1), random.randint(0, len(self.real_data) - 1)
-        doodle_data = self.doodle_preprocess(self.doodle_data[doodle_idx])
-        real_data = self.real_preprocess(self.real_data[real_idx])
-        return doodle_data, self.doodle_label[doodle_idx], real_data, self.real_label[real_idx]
+        # sample label first so that doodle and real data belong to the same category
+        label = random.choice(list(self.label_idx.keys()))
+        doodle_data = self.doodle_preprocess(random.choice(self.doodle_dict[label]))
+        real_data = self.real_preprocess(random.choice(self.real_dict[label]))
+        numer_label = self.label_idx[label]
+        return doodle_data, numer_label, real_data, numer_label
 
     def __len__(self):
         return max(len(self.doodle_data), len(self.real_data))     # could be arbitrary number
 
 
 if __name__ == '__main__':
-    dataset = ImageDataset(64, 32)
-    print('finished!')
+    dataset = ImageDataset(64, 32, train=True)
+    print('done!')
