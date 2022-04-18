a0d2bea1510646dcb04cd15108d7eb54924832ec
Bo Ai <albert_0110@outlook.com>
2022-04-18 10:31:37 +0800

    added merging multiple datasets

--- dataset.py
+++ dataset.py
@@ -3,49 +3,70 @@ from torchvision import transforms
 import torch
 import random
 import numpy as np
+import cv2
+
+
+def combined_dataset(datasets, size):
+    combined_dataset = {}
+    for name, dataset in datasets.items():
+        for class_name, class_data in dataset.items():
+            if class_name not in combined_dataset:
+                combined_dataset[class_name] = []
+            # resize data so they can be stacked
+            resized = []
+            for data in class_data:
+                resized.append(cv2.resize(data, (size, size), interpolation=cv2.INTER_AREA))
+            resized = np.stack(resized, axis=0)
+            combined_dataset[class_name].append(resized)
+    for class_name, lst_datasets in combined_dataset.items():
+        combined_dataset[class_name] = np.concatenate(lst_datasets, axis=0)
+    return combined_dataset
 
 
 class ImageDataset(Dataset):
     DATASET_DIR = {True: 'dataset/dataset_train.npy', False: 'dataset/dataset_test.npy'}
-    DOODLE_KEY = 'sketchy_doodle'  # 'google_doodles'
-    REAL_KEY = 'sketchy_real'   # 'google_real'
 
-    def __init__(self, doodle_size, real_size, train: bool):
+    def __init__(self, doodles_list, real_list, doodle_size, real_size, train: bool):
         super(ImageDataset, self).__init__()
+
         dataset = np.load(self.DATASET_DIR[train], allow_pickle=True)[()]
-        doodle, real = dataset[self.DOODLE_KEY], dataset[self.REAL_KEY]
-        self.doodle_dict = doodle
-        self.real_dict = real
+
+        doodle_datasets = {name: data for name, data in dataset.items() if name in doodles_list}
+        real_datasets = {name: data for name, data in dataset.items() if name in real_list}
+        self.doodle_dict = combined_dataset(doodle_datasets, doodle_size)
+        self.real_dict = combined_dataset(real_datasets, real_size)
 
         # sanity check
-        assert doodle.keys() == real.keys(), f'doodle and real images label classes do not match'
+        assert set(self.doodle_dict.keys()) == set(self.real_dict.keys()), \
+            f'doodle and real images label classes do not match'
 
         # process classes
         label_idx = {}
-        for key in doodle.keys():
+        for key in self.doodle_dict.keys():
             if key not in label_idx:
                 label_idx[key] = len(label_idx)
         self.label_idx = label_idx
 
         # parse data and labels
-        self.doodle_data, self.doodle_label = self._return_x_y_pairs(doodle, label_idx)
-        self.real_data, self.real_label = self._return_x_y_pairs(real, label_idx)
+        self.doodle_data, self.doodle_label = self._return_x_y_pairs(self.doodle_dict, label_idx)
+        self.real_data, self.real_label = self._return_x_y_pairs(self.real_dict, label_idx)
 
         # data preprocessing
         self.doodle_preprocess = transforms.Compose([
             transforms.ToPILImage(),
             transforms.Resize(doodle_size),
             transforms.ToTensor(),
-            transforms.Normalize(self.doodle_data.mean(), self.doodle_data.std())
+            transforms.Normalize((self.doodle_data/255).mean(), (self.doodle_data/255).std())   # IMPORTANT / 255
         ])
+
         self.real_preprocess = transforms.Compose([
             transforms.ToPILImage(),
             transforms.Resize(real_size),
             transforms.ToTensor(),
-            transforms.Normalize(self.real_data.mean(axis=(0,1,2)), self.real_data.std(axis=(0,1,2)))
+            transforms.Normalize((self.real_data/255).mean(axis=(0, 1, 2)), (self.real_data/255).std(axis=(0, 1, 2)))
         ])
 
-        print(f'Dataset {self.DOODLE_KEY} and {self.REAL_KEY}. '
+        print(f'Train = {train}. Doodle list: {doodles_list}, \n real list: {real_list}. \n classes: {label_idx.keys()} \n'
               f'Doodle data size {len(self.doodle_data)}, real data size {len(self.real_data)}, '
               f'ratio {len(self.doodle_data)/len(self.real_data)}')
 
@@ -72,5 +93,10 @@ class ImageDataset(Dataset):
 
 
 if __name__ == '__main__':
-    dataset = ImageDataset(64, 32, train=True)
-    print('done!')
+    doodles = ['sketchy_doodle', 'tuberlin', 'google_doodles']
+    reals = ['sketchy_real', 'google_real', 'cifar']
+    doodle_size = 64
+    real_size = 64
+    train_set = ImageDataset(doodles, reals, doodle_size, real_size, train=True)
+    val_set = ImageDataset(doodles, reals, doodle_size, real_size, train=False)
+    print(len(train_set[0]))
