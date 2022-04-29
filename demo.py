41ceb6094d51f107638b9429b9d331a88d3f89d9
rishabh anand <rish@rishabhs-MacBook-Pro.local>
2022-04-29 15:50:45 +0800

    Add gradio working demo files

--- demo.py
+++ demo.py
@@ -1,7 +1,12 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import gradio as gr
+import matplotlib.pyplot as plt
+from torchvision.utils import save_image
 from engine import Engine
 from model_training import *
+from torchvision import transforms
 
 def convbn(in_channels, out_channels, kernel_size, stride, padding, bias):
     return nn.Sequential(
@@ -20,10 +25,6 @@ class CNN(nn.Module):
         layer3 = convbn(self.CHANNELS[2], self.CHANNELS[3], kernel_size=3, stride=2, padding=1, bias=True)
         layer4 = convbn(self.CHANNELS[3], self.CHANNELS[4], kernel_size=3, stride=2, padding=1, bias=True)
         pool = nn.AdaptiveAvgPool2d(self.POOL)
-        layer1_2 = convbn(self.CHANNELS[1], self.CHANNELS[1], kernel_size=3, stride=1, padding=0, bias=True)
-        layer2_2 = convbn(self.CHANNELS[2], self.CHANNELS[2], kernel_size=3, stride=1, padding=0, bias=True)
-        layer3_2 = convbn(self.CHANNELS[3], self.CHANNELS[3], kernel_size=3, stride=1, padding=0, bias=True)
-        layer4_2 = convbn(self.CHANNELS[4], self.CHANNELS[4], kernel_size=3, stride=1, padding=0, bias=True)
         self.layers = nn.Sequential(layer1, layer2, layer3, layer4, pool)
         self.nn = nn.Linear(self.POOL[0] * self.POOL[1] * self.CHANNELS[4], n_classes)
         self.dropout = nn.Dropout(p=dropout)
@@ -32,9 +33,14 @@ class CNN(nn.Module):
         x = self.nn(self.dropout(feats))
         if return_feats:
             return x, feats
-        return x
+        return x        
 
-def doodle_search(img, model_choice, topk):
+doodle_model = CNN(1, 9)
+real_model = CNN(3, 9)
+real_val_set = RealDataset(train=False)
+engine = Engine(real_val_set, doodle_model, real_model)
+
+def doodle_search(img, topk):
     pretrained_paths = {
         "V1 MLP": ("", ""),
         "V2 CNN": ("", ""),
@@ -43,36 +49,41 @@ def doodle_search(img, model_choice, topk):
         "V4 ConvNeXT + Multi-contrastive": ("", ""),
     }
 
-    models = {
-        "V1 MLP": (None, None),
-        "V2 CNN": (None, None),
-        "V3 CNN + Contrastive": (None, None),
-        "V4 CNN + Multi-contrastive": (None, None),
-        "V4 ConvNeXT + Multi-contrastive": (None, None),
-    }
-
-    doodle_path, real_path = pretrained_paths[model_choice]
-    doodle_model_class, real_model_class = models[model_choice]
-    # doodle_model = load_model(doodle_model_class, doodle_path)
-    # real_model = load_model(real_model_class, real_path)
-
-    doodle_model = CNN(1, 9)
-    real_model = CNN(3, 9)
-
-    real_val_set = RealDataset(train=False)
-    engine = Engine(real_val_set, doodle_model, real_model)
     results = engine.query(img, topk=topk)
 
-    return results
+    output_transform = transforms.Compose([
+        transforms.ToTensor()
+    ]) 
+
+    model = nn.Sequential(nn.Identity())
+    output_tensor = torch.Tensor(topk, 3, 64, 64)
+    for i, img in enumerate(results):
+        output_tensor[i, :, :, :] = output_transform(img)
+
+    save_image(output_tensor, "output.png", normalize=True)
+
+    return 'output.png'
+
+text = """
+## Welcome to _Doogle_! 
+
+Group 1 presents this reverse-image search engine that
+takes in doodles and returns the best-matching real-life images to address 
+your tip-of-the-tongue problem :D
+
+**Note**: Please wait for database to be indexed when the app first loads up. Depending on the hardware you have, this might take a few minutes. Clearly, we are not able to beat Google's speeds :')
+
+---
+
+Work by: **Group 1**, Ai Bo, New Jun Jie, and Rishabh Anand
+"""
 
 demo = gr.Interface(
     fn=doodle_search,
+    title="Doogle",
+    article=text,
     inputs=[
         gr.inputs.Image(shape=(64, 64), image_mode="L", invert_colors=False, source="canvas"), 
-        gr.inputs.Dropdown(
-            ["V1 MLP", "V2 CNN", "V3 CNN + Contrastive", "V4 CNN + Multi-contrastive", "V5 ConvNeXT + Multi-contrastive"], 
-            label="Choose your model"
-        ),
         gr.inputs.Slider(minimum=5, maximum=20, step=1, label="Top K Best Matches")
     ],  
     outputs="image")
